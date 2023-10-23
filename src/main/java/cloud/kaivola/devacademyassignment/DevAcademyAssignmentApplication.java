82dfcf3a392802955a1ae41044a91d26b8689a41
vkaiv <ville.kaivola@gmail.com>
2023-10-23 17:27:59 +0300

    initial commit

---
+++ src/main/java/cloud/kaivola/devacademyassignment/DevAcademyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package cloud.kaivola.devacademyassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAcademyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAcademyAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/kaivola/devacademyassignment/DevAcademyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.kaivola.devacademyassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAcademyAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
