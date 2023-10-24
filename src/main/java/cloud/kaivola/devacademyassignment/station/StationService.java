a34222dcc5d0b9498cbe5b96482bfd27c60f9149
vkaiv <ville.kaivola@gmail.com>
2023-10-24 17:25:12 +0300

    statistics are now included in single station requests

--- src/main/java/cloud/kaivola/devacademyassignment/station/StationService.java
+++ src/main/java/cloud/kaivola/devacademyassignment/station/StationService.java
@@ -1,5 +1,7 @@
 package cloud.kaivola.devacademyassignment.station;
 
+import cloud.kaivola.devacademyassignment.statistics.StationStatistics;
+import cloud.kaivola.devacademyassignment.statistics.StatisticsService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -8,17 +10,21 @@ import org.springframework.stereotype.Service;
 public class StationService {
 
     private final StationRepository stationRepository;
+    private final StatisticsService statisticsService;
 
-    public StationService(StationRepository stationRepository) {
+    public StationService(StationRepository stationRepository, StatisticsService statisticsService) {
         this.stationRepository = stationRepository;
+        this.statisticsService = statisticsService;
     }
 
     public Page<Station> getStations(Integer page) {
         return stationRepository.findAll(PageRequest.of(page, 30));
     }
 
-    public Station getStationById(Long id) {
-        return stationRepository.findById(id).orElseThrow();
+    public Station getStationById(Integer id) {
+        Station station = stationRepository.findById(id).orElseThrow();
+        StationStatistics statistics = statisticsService.getStationStatisticsById(station.getId());
+        station.setStatistics(statistics);
+        return station;
     }
-
 }
