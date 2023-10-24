e9fbb09158db87031612cd23d4a146de93d43233
vkaiv <ville.kaivola@gmail.com>
2023-10-24 16:40:13 +0300

    added methods for fetching Journey data from the db

--- src/main/java/cloud/kaivola/devacademyassignment/journey/JourneyRepository.java
+++ src/main/java/cloud/kaivola/devacademyassignment/journey/JourneyRepository.java
@@ -1,6 +1,16 @@
 package cloud.kaivola.devacademyassignment.journey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JourneyRepository  extends JpaRepository<Journey, Integer> {
+    List<Journey> findAllByDepartureStationId(Integer id);
+
+    List<Journey> findAllByReturnStationId(Integer id);
+
+    @Query(value = "SELECT COUNT(id) FROM journey WHERE return_station_id = :stationId", nativeQuery = true)
+    Integer getNumOfEndingJourneysByStationId(@Param("stationId") Integer stationId);
 }
