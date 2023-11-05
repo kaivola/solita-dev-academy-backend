9ff0eab30455e6eb1b31552a6efeaae3cfacdf5a
vkaiv <ville.kaivola@gmail.com>
2023-11-05 16:45:42 +0200

    updated tests for StationService

--- src/test/java/cloud/kaivola/devacademyassignment/StationServiceTest.java
+++ src/test/java/cloud/kaivola/devacademyassignment/StationServiceTest.java
@@ -9,7 +9,7 @@ import cloud.kaivola.devacademyassignment.statistics.StatisticsService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 public class StationServiceTest {
@@ -25,12 +25,52 @@ public class StationServiceTest {
 
     @Test
     void shouldFormatDistanceAndDurationCorrectly() {
-        StationStatistics statistics1 = new StationStatistics(10, 50, 150.99999, 1.0001000);
+        StationStatistics statistics1 = new StationStatistics(10, 50, 150.9, 1.0001000);
         Station s1 = new Station(1, "Station 1", "Address 1", "0.0", "0.0", statistics1);
 
-        StationDto dto1 = stationService.mapStationToStationDto(s1);
-        assertEquals("151.0", dto1.averageDistanceOfJourneys());
-        assertEquals(1, dto1.averageDurationOfJourneys());
+        StationDto dto1 = stationService.mapStationToDto(s1);
+        assertEquals(150.9, dto1.getAverageDistanceOfJourneys());
+        assertEquals(1, dto1.getAverageDurationOfJourneys());
     }
 
+    @Test
+    void shouldConvertCoordinatesToFloats() {
+        Station s1 = new Station(1, "Station 1", "Address 1", "61.0", "25.0", null);
+        StationDto dto1 = stationService.mapStationToDto(s1);
+
+        assertEquals(61.0, dto1.getCoordinateX());
+        assertEquals(25.0, dto1.getCoordinateY());
+    }
+
+    @Test
+    void shouldMapStationToDto() {
+        StationStatistics statistics1 = new StationStatistics(10, 50, 150.9, 1.0001000);
+        Station s1 = new Station(1, "Station 1", "Address 1", "0.0", "0.0", statistics1);
+        StationDto dto1 = stationService.mapStationToDto(s1);
+        assertEquals(s1.getId(), dto1.getId());
+        assertEquals(s1.getStationName(), dto1.getName());
+        assertEquals(s1.getStationAddress(), dto1.getAddress());
+        assertNotNull(dto1.getCoordinateY());
+        assertNotNull(dto1.getCoordinateX());
+        assertNotNull(dto1.getNumOfJourneysStarting());
+        assertNotNull(dto1.getNumOfJourneysEnding());
+        assertNotNull(dto1.getAverageDistanceOfJourneys());
+        assertNotNull(dto1.getAverageDurationOfJourneys());
+    }
+
+
+    @Test
+    void shouldMapStationToDtoWithoutStatistics() {
+        Station s1 = new Station(1, "Station 1", "Address 1", "0.0", "0.0", null);
+        StationDto dto1 = stationService.mapStationToDto(s1);
+        assertEquals(s1.getId(), dto1.getId());
+        assertEquals(s1.getStationName(), dto1.getName());
+        assertEquals(s1.getStationAddress(), dto1.getAddress());
+        assertNotNull(dto1.getCoordinateY());
+        assertNotNull(dto1.getCoordinateX());
+        assertNull(dto1.getNumOfJourneysStarting());
+        assertNull(dto1.getNumOfJourneysEnding());
+        assertNull(dto1.getAverageDistanceOfJourneys());
+        assertNull(dto1.getAverageDurationOfJourneys());
+    }
 }
